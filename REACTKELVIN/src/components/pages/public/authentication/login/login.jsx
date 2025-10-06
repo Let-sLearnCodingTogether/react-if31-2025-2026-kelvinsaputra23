cb547694c28da6d9ae0ea6e697717d44a0b7625d
FernandoFeliansyah <fernandofeliansyah_2226250051@mhs.mdp.ac.id>
2025-10-06 20:40:45 +0700

    x

---
+++ REACTKELVIN/src/components/pages/public/authentication/login/login.jsx
@@ -0,0 +1,17 @@
+export default function login (){
+    return (
+        <div className="grid min-h-screen grid -cols-1 md:grid-cols-2">
+            <div className="hidden bg-image p-5 md:block">
+                <div className="
+                
+                ">
+
+                </div>
+
+            </div>
+    
+        </div>
+    )
+
+
+}
