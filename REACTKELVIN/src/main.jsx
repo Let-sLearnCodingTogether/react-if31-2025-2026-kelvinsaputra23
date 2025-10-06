3a5befdd4df75298f812802a7ff6f82d8b1a210d
FernandoFeliansyah <fernandofeliansyah_2226250051@mhs.mdp.ac.id>
2025-10-06 20:01:04 +0700

    x

--- REACTKELVIN/src/main.jsx
+++ REACTKELVIN/src/main.jsx
@@ -5,7 +5,22 @@ import "./assets/css/style.css"
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <div className=" bg-orange-500">
+      
       welcome
     </div>
+  
+    <div>
+        <UserProfile/>
+
+        <UserProfile 
+        avatar = "https://avatar.iran.liara.run/public/29"
+        username= "kelvin"
+        email='test@email.com'
+
+        />
+        
+      </div>
+
+      
   </StrictMode>,
 )
