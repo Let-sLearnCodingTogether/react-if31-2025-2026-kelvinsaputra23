c6a6095cac56609fb63149a1134042e66becabc9
kelvinsaputra23 <kelvinsaputra_2327250006@mhs.mdp.ac.id>
2025-10-08 22:56:16 -1100

    .

--- REACTKELVIN/src/main.jsx
+++ REACTKELVIN/src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import "./assets/css/styles.css"
-import Login from './pages/public/authentication/login/login';
+import Login from './pages/public/authentication/login/login'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Login />
-  </StrictMode>,
+  </StrictMode>
 )
--- REACTKELVIN/vite.config.js
+++ REACTKELVIN/vite.config.js
@@ -1,11 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss()
-  ],
-})
+  plugins: [react()],
+  server: {
+    port: 3000,       // Ganti sesuai port yang kamu inginkan
+    strictPort: true, // Supaya jika port sudah dipakai, Vite akan error dan tidak auto-cari port lain
+  },
+});
