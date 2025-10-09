31a6fa6e745bf3a35be8be0982327091e34fa319
kelvinsaputra23 <kelvinsaputra_2327250006@mhs.mdp.ac.id>
2025-10-08 22:17:52 -1100

    k

--- REACTKELVIN/src/main.jsx
+++ REACTKELVIN/src/main.jsx
@@ -1,11 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import "./assets/css/style.css"
+import "./assets/css/styles.css"
+import Login from './pages/public/authentication/login/login';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <div>
-      ok
-    </div>
+    <Login />
   </StrictMode>,
 )
